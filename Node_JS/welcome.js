8c6548117fefadd79d7cb022a9a665d9061c5ff1
shashank09234 <shashank09234@gmail.com>
2019-07-22 09:20:58 +0530

    Node Js

---
+++ Node_JS/welcome.js
@@ -0,0 +1,15 @@
+let greet = () =>{
+    console.log("Welcome to node");
+};
+greet();
+
+let employee = ()=> {
+	name: 'Shashank';
+	age: '23';
+	course: 'MCA';
+	college: 'Nalanda College';
+	address: {
+		city: 'Biharshrif';
+		
+	}
+}
